232f3dac7b291aa73d6738812e935c895f2efb4d
BuildTools <unconfigured@null.spigotmc.org>
2026-09-09 18:26:43 +1200

    async storage implementation wip

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,10 +1,25 @@
 import { Button, Text } from '@react-navigation/elements';
 import { StyleSheet, View } from 'react-native';
 
+import { createAsyncStorage } from "@react-native-async-storage/async-storage";
+
+// create a storage instance
+const storage = createAsyncStorage("appDB");
+
+async function demo() {
+  await storage.setItem("userToken", "abc123");
+
+  const token = await storage.getItem("userToken");
+  console.log("Stored token:", token); // abc123
+
+  await storage.removeItem("userToken");
+}
+demo()
+
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text>Find My Socks</Text>
       <Text>Open up 'src/App.tsx' to start working on your app!</Text>
       <Button screen="Profile" params={{ user: 'jane' }}>
         Go to Profile
