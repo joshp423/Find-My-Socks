4f2cbdab95aaabbf2f754234483c2617085b6f7d
BuildTools <unconfigured@null.spigotmc.org>
2026-09-15 12:41:02 +1200

    added expo sqlite for creating db for userData

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,22 +1,35 @@
 import { Button, Text } from '@react-navigation/elements';
 import { StyleSheet, View } from 'react-native';
 import { CreateProfile } from './CreateProfile';
-
 import { createAsyncStorage } from "@react-native-async-storage/async-storage";
+import * as SQLite from 'expo-sqlite';
 import { useEffect, useState } from 'react';
 
 // create a storage instance
 export const userStorage = createAsyncStorage("appDB");
 
-// async function demo() {
-//   await userStorage.setItem("userToken", "abc123");
+//create db and table if not already
+const db = await SQLite.openDatabaseAsync('databaseName');
 
-//   const token = await userStorage.getItem("userToken");
-//   console.log("Stored token:", token); // abc123
+await db.execAsync(`
+  CREATE TABLE IF NOT EXISTS userContainers (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name STRING NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS userCompartments (
+    id INTEGER PRIMARY KEY NOT NULL,
+    parentID INTEGER NOT NULL,
+    name STRING NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS userItems (
+    id INTEGER PRIMARY KEY NOT NULL,
+    parentID INTEGER NOT NULL,
+    name STRING NOT NULL,
+    amount INTEGER (amount >= 0 AND amount <= 100) NOT NULL
+  );
+`);
 
 //   await userStorage.removeItem("userToken");
-// }
-// demo()
 
 export function Home() {
   const [userProfileCheck, setUserProfileCheck] = useState<boolean>(false);
